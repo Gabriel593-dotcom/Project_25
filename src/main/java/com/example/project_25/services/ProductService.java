a798b641a305a1472091fd39e2633c2659fd33c8
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2021-11-27 16:40:36 -0300

    Associacao um para muitos entre entidade Product e OrderItem

--- src/main/java/com/example/project_25/services/ProductService.java
+++ src/main/java/com/example/project_25/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.project_25.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,16 @@ import com.example.project_25.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return productRepository.findAll();
+	}
+
+	public Product finById(Long id) {
+		Optional<Product> obj = productRepository.findById(id);
+		return obj.get();
 	}
 }
